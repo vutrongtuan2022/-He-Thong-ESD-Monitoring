20835656c7d19aabac7a39f54b568caacd2da835
truongdb <engtruong1609@gmail.com>
2024-05-18 17:31:55 +0700

    update

---
+++ src/components/pages/profile/MainEditProfile/MainEditProfile.tsx
@@ -0,0 +1,309 @@
+import React, {useState} from 'react';
+
+import {PropsMainEditProfile} from './interfaces';
+import styles from './MainEditProfile.module.scss';
+import WrapperContainer from '~/components/layouts/WrapperContainer';
+import {BsThreeDots} from 'react-icons/bs';
+import Breadcrumb from '~/components/common/Breadcrumb';
+import {PATH} from '~/constants/config';
+import Button from '~/components/common/Button';
+import Form, {Input} from '~/components/common/Form';
+import clsx from 'clsx';
+import Select, {Option} from '~/components/common/Select';
+import {GENDER} from '~/constants/config/enum';
+import DatePicker from '~/components/common/DatePicker';
+import AvatarChange from '~/components/common/AvatarChange';
+
+function MainEditProfile({}: PropsMainEditProfile) {
+	const [form, setForm] = useState<any>({
+		avatar: '',
+		date: '',
+		name: '',
+		email: '',
+		phone: '',
+		province: '',
+		district: '',
+		ward: '',
+		address: '',
+		role: '',
+		position: '',
+		gender: null,
+	});
+
+	const handleSubmit = () => {};
+
+	return (
+		<div className={styles.container}>
+			<Breadcrumb
+				listUrls={[
+					{
+						path: PATH.Home,
+						title: 'Trang chủ',
+					},
+					{
+						path: '',
+						title: 'Chỉnh sửa tài khoản',
+					},
+				]}
+				action={
+					<div className={styles.main_action}>
+						<div className={styles.box_icon}>
+							<BsThreeDots className={styles.dots} color='#23262f' size={20} />
+						</div>
+					</div>
+				}
+			/>
+			<WrapperContainer>
+				<div className={styles.wrapper}>
+					<div className={styles.header}>
+						<div className={styles.left}>
+							<h4>Chỉnh sửa người dùng</h4>
+							<p>Điền đầy đủ các thông tin người dùng</p>
+						</div>
+						<div className={styles.right}>
+							<Button href={PATH.Profile} p_10_24 rounded_2 grey_outline>
+								Hủy bỏ
+							</Button>
+							<Button p_10_24 rounded_2 primary>
+								Cập nhật
+							</Button>
+						</div>
+					</div>
+
+					<div className={styles.form}>
+						<Form form={form} setForm={setForm}>
+							<div className={'mb'}>
+								<AvatarChange path={form?.avatar} name='avatar' />
+							</div>
+							<Input
+								name='name'
+								value={form.name || ''}
+								label={
+									<span>
+										Họ và tên <span style={{color: 'red'}}>*</span>
+									</span>
+								}
+								placeholder='Nhập tên người dùng'
+							/>
+							<div className={clsx('mt', 'col_2')}>
+								<Input
+									name='email'
+									value={form.email || ''}
+									label={
+										<span>
+											Email <span style={{color: 'red'}}>*</span>
+										</span>
+									}
+									placeholder='Nhập email'
+								/>
+								<div>
+									<Input
+										name='phone'
+										value={form.phone || ''}
+										label={
+											<span>
+												Số diện thoại<span style={{color: 'red'}}>*</span>
+											</span>
+										}
+										placeholder='Nhập số điện thoại'
+									/>
+								</div>
+							</div>
+
+							<div className={clsx('mt', 'col_2')}>
+								<DatePicker
+									icon={true}
+									label={'Ngày sinh'}
+									placeholder='Chọn ngày sinh'
+									value={form.date}
+									onSetValue={(date) =>
+										setForm((prev: any) => ({
+											...prev,
+											date: date,
+										}))
+									}
+									name='dateOfBirth'
+									onClean={true}
+								/>
+								<div className={styles.gennder}>
+									<label>Giới tính</label>
+									<div className={styles.group_radio}>
+										<div className={styles.item_radio}>
+											<input
+												id='male'
+												className={styles.input_radio}
+												type='radio'
+												name='gender'
+												value={form.gender}
+												checked={form.gender == GENDER.NAM}
+												onChange={(e) =>
+													setForm((prev: any) => ({
+														...prev,
+														gender: GENDER.NAM,
+													}))
+												}
+											/>
+											<label className={styles.input_lable} htmlFor='male'>
+												Nam
+											</label>
+										</div>
+
+										<div className={styles.item_radio}>
+											<input
+												id='female'
+												className={styles.input_radio}
+												type='radio'
+												name='gender'
+												value={form.gender}
+												checked={form.gender == GENDER.NU}
+												onChange={(e) =>
+													setForm((prev: any) => ({
+														...prev,
+														gender: GENDER.NU,
+													}))
+												}
+											/>
+											<label className={styles.input_lable} htmlFor='female'>
+												Nữ
+											</label>
+										</div>
+
+										<div className={styles.item_radio}>
+											<input
+												id='other'
+												className={styles.input_radio}
+												type='radio'
+												name='gender'
+												value={form.gender}
+												checked={form.gender == GENDER.KHAC}
+												onChange={(e) =>
+													setForm((prev: any) => ({
+														...prev,
+														gender: GENDER.KHAC,
+													}))
+												}
+											/>
+											<label className={styles.input_lable} htmlFor='other'>
+												Khác
+											</label>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div className={clsx('col_2', 'mt')}>
+								<Select
+									isSearch
+									name='position'
+									value={form.position}
+									placeholder='Chức vụ'
+									onChange={(position) =>
+										setForm((prev: any) => ({
+											...prev,
+											position: position,
+										}))
+									}
+									label={<span>Chức vụ</span>}
+								>
+									<Option title='Chức vụ 1' value={1} />
+									<Option title='Chức vụ 2' value={2} />
+								</Select>
+								<Select
+									isSearch
+									name='role'
+									value={form.role}
+									placeholder='Vai trò'
+									onChange={(role) =>
+										setForm((prev: any) => ({
+											...prev,
+											role: role,
+										}))
+									}
+									label={<span>Vai trò</span>}
+								>
+									<Option title='Vai trò 1' value={1} />
+									<Option title='Vai trò 2' value={2} />
+								</Select>
+							</div>
+
+							<div className={clsx('mt', 'col_3')}>
+								<Select
+									isSearch
+									name='province'
+									value={form.province}
+									placeholder='Chọn tỉnh, thành phố'
+									onChange={(province) =>
+										setForm((prev: any) => ({
+											...prev,
+											province: province,
+										}))
+									}
+									label={
+										<span>
+											Tỉnh/Thành phố<span style={{color: 'red'}}>*</span>
+										</span>
+									}
+								>
+									<Option title='Hà Nôi' value={1} />
+									<Option title='Hà Tĩnh' value={2} />
+								</Select>
+								<div>
+									<Select
+										isSearch
+										name='district'
+										value={form.district}
+										placeholder='Chọn quận/huyện'
+										onChange={(district) =>
+											setForm((prev: any) => ({
+												...prev,
+												district: district,
+											}))
+										}
+										label={
+											<span>
+												Quận/Huyện<span style={{color: 'red'}}>*</span>
+											</span>
+										}
+									>
+										<Option title='Ba Đình' value={1} />
+										<Option title='Đống Đa' value={2} />
+									</Select>
+								</div>
+								<Select
+									isSearch
+									name='ward'
+									value={form.ward}
+									placeholder='Chọn xã, phường'
+									onChange={(ward) =>
+										setForm((prev: any) => ({
+											...prev,
+											ward: ward,
+										}))
+									}
+									label={
+										<span>
+											Xã/phường<span style={{color: 'red'}}>*</span>
+										</span>
+									}
+								>
+									<Option title='Trâu Quỳ' value={1} />
+									<Option title='Đặng Xá' value={2} />
+								</Select>
+							</div>
+							<div className={clsx('mt')}>
+								<Input
+									name='address'
+									value={form.address || ''}
+									label={<span>Địa chỉ chi tiết</span>}
+									placeholder='Nhập địa chỉ chi tiết'
+								/>
+							</div>
+						</Form>
+					</div>
+				</div>
+			</WrapperContainer>
+		</div>
+	);
+}
+
+export default MainEditProfile;
---
+++ src/components/pages/profile/MainProfile/MainProfile.tsx
@@ -0,0 +1,179 @@
+import React, {useState} from 'react';
+import Image from 'next/image';
+
+import {PropsMainProfile} from './interfaces';
+import styles from './MainProfile.module.scss';
+import WrapperContainer from '~/components/layouts/WrapperContainer';
+import {BsThreeDots} from 'react-icons/bs';
+import Breadcrumb from '~/components/common/Breadcrumb';
+import {PATH} from '~/constants/config';
+import Link from 'next/link';
+import {IoArrowBackOutline} from 'react-icons/io5';
+import Button from '~/components/common/Button';
+import icons from '~/constants/images/icons';
+import ImageFill from '~/components/common/ImageFill';
+import {FaCalendarCheck, FaMapMarkerAlt} from 'react-icons/fa';
+import {IoMdMail} from 'react-icons/io';
+import Form, {FormContext, Input} from '~/components/common/Form';
+import {ShieldSecurity} from 'iconsax-react';
+import clsx from 'clsx';
+
+function MainProfile({}: PropsMainProfile) {
+	const [form, setForm] = useState<any>({oldPass: '', newPass: ''});
+
+	const handleSubmit = () => {};
+
+	return (
+		<div className={styles.container}>
+			<Breadcrumb
+				listUrls={[
+					{
+						path: PATH.Home,
+						title: 'Trang chủ',
+					},
+					{
+						path: '',
+						title: 'Chi tiết tài khoản',
+					},
+				]}
+				action={
+					<div className={styles.main_action}>
+						<div className={styles.box_icon}>
+							<BsThreeDots className={styles.dots} color='#23262f' size={20} />
+						</div>
+					</div>
+				}
+			/>
+			<WrapperContainer>
+				<div className={styles.wrapper}>
+					<div className={styles.header}>
+						<div>
+							<Link href={PATH.Home} className={styles.header_title}>
+								<IoArrowBackOutline fontSize={24} fontWeight={600} />
+								<p>Chi tiết tài khoản</p>
+							</Link>
+							<p className={styles.des}>Chào mừng Dương Mĩ linh đến với hệ thống quản lý Thái Hưng</p>
+						</div>
+						<div className={styles.list_btn}>
+							<Button
+								className={styles.btn}
+								rounded_4
+								w_fit
+								p_10_24
+								primary
+								bold
+								icon={<Image alt='icon import' src={icons.icon_edit} width={20} height={20} />}
+								href={`${PATH.Profile}/123`}
+							>
+								Cập nhật
+							</Button>
+						</div>
+					</div>
+
+					<div className={styles.main}>
+						<div className={styles.box_info}>
+							<ImageFill src={''} alt='anh dai dien' className={styles.avatar} />
+							<div className={styles.info}>
+								<div className={styles.name}>
+									<h5>Dương Mĩ Linh</h5>
+									<div className={styles.role}>Chức vụ</div>
+								</div>
+								<div className={styles.item}>
+									<div className={styles.icon}>
+										<FaCalendarCheck />
+									</div>
+									<p>24/08/1986</p>
+								</div>
+								<div className={styles.item}>
+									<div className={styles.icon}>
+										<IoMdMail />
+									</div>
+									<p>Duongmilinh23@gmail.com</p>
+								</div>
+								<div className={styles.item}>
+									<div className={styles.icon}>
+										<FaMapMarkerAlt />
+									</div>
+									<p>Số 219 Đường Nguyễn Văn Cừ, Phường Hồng Hà, Thành phố Hạ Long, Tỉnh Quảng Ninh</p>
+								</div>
+							</div>
+						</div>
+
+						<div className={styles.line}></div>
+
+						<div className={styles.changePass}>
+							<h5>Thay đổi mật khẩu</h5>
+							<p>Bạn có thể thay đổi mật khẩu theo ý muốn của bạn. Mật khẩu thay đổi không trùng với mật khẩu cũ.</p>
+
+							<div className={styles.form}>
+								<Form form={form} setForm={setForm} onSubmit={handleSubmit}>
+									<Input
+										type='password'
+										name='oldPass'
+										value={form?.oldPass}
+										placeholder='Nhập mật khẩu cũ'
+										onClean
+										isRequired
+										icon={<ShieldSecurity size='20' variant='Bold' />}
+										label={
+											<span>
+												Mật khẩu cũ <span style={{color: 'red'}}>*</span>
+											</span>
+										}
+									/>
+
+									<div className={clsx('mt', 'col_2')}>
+										<Input
+											type='password'
+											name='newPass'
+											value={form?.newPass}
+											placeholder='Nhập mật khẩu mới'
+											onClean
+											isRequired
+											icon={<ShieldSecurity size='20' variant='Bold' />}
+											label={
+												<span>
+													Nhập mật khẩu mới <span style={{color: 'red'}}>*</span>
+												</span>
+											}
+										/>
+
+										<div>
+											<Input
+												type='password'
+												name='newPass'
+												value={form?.newPass}
+												valueConfirm={form?.newPass}
+												placeholder='Nhập lại mật khẩu'
+												onClean
+												isRequired
+												icon={<ShieldSecurity size='20' variant='Bold' />}
+												label={
+													<span>
+														Xác nhận mật khẩu mới <span style={{color: 'red'}}>*</span>
+													</span>
+												}
+											/>
+										</div>
+									</div>
+
+									<div className={styles.btn}>
+										<FormContext.Consumer>
+											{({isDone}) => (
+												<Button primary bold rounded_8 disable={!isDone}>
+													Đổi mật khẩu
+												</Button>
+											)}
+										</FormContext.Consumer>
+									</div>
+								</Form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</WrapperContainer>
+		</div>
+	);
+}
+
+export default MainProfile;
