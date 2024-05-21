f3ac30eb68c8aff07472223663b3ba3bb920618c
truongdb <engtruong1609@gmail.com>
2024-05-21 17:13:58 +0700

    ff

--- src/components/pages/bo-phat/HistoryTransmitter/interfaces/index.ts
+++ src/components/pages/bo-phat/HistoryTransmitter/interfaces/index.ts
@@ -1,1 +1,9 @@
 export interface PropsHistoryTransmitter {}
+
+export interface IDeviceNGHistory {
+	teamCode: string;
+	teamName: string;
+	timeNgStart: string;
+	edsStatic: number;
+	totalNgMinutes: number;
+}
