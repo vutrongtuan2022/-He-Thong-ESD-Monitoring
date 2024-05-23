07e76979d95f621ab33c38ab9b93fbb9d41d1392
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-05-23 10:45:56 +0700

    uupdate

--- src/components/pages/nhan-vien/MainPageUpdateStaff/interfaces/index.ts
+++ src/components/pages/nhan-vien/MainPageUpdateStaff/interfaces/index.ts
@@ -1,1 +1,22 @@
-export interface PropsMainPageUpdateStaff {}
+import {STATUS_GENERAL} from '~/constants/config/enum';
+import {IUser} from '../../ListTransmitter/interfaces';
+
+export interface PropsMainPageUpdateStaff {
+	dataUpdate: IUser;
+}
+
+export interface IFormUpdate {
+	uuid: string;
+	userName: string;
+	fullname: string;
+	teamUuid: string;
+	code: string;
+	gender: number;
+	email: string;
+	phone: string;
+	address: string;
+	birthday: string;
+	avatar: string;
+	role: string;
+	status: STATUS_GENERAL;
+}
