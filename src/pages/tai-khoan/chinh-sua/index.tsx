ae30a37322b2c5531eb81844a75602164707ec21
TuanVT <vutrongtuan2022@gmail.com>
2024-05-24 09:00:37 +0700

    update

--- src/pages/tai-khoan/chinh-sua/index.tsx
+++ src/pages/tai-khoan/chinh-sua/index.tsx
@@ -1,23 +1,23 @@
 import Head from 'next/head';
 import {Fragment, ReactElement} from 'react';
 import BaseLayout from '~/components/layouts/BaseLayout';
-import MainPageUser from '~/components/pages/nguoi-dung/MainPageUser';
+import UpdateAccount from '~/components/pages/tai-khoan/UpdatePageAccount';
 
 export default function Page() {
 	return (
 		<Fragment>
 			<Head>
-				<title>Quản lý người dùng</title>
-				<meta name='description' content='Quản lý người dùng' />
+				<title>Chỉnh sửa tài khoản</title>
+				<meta name='description' content='Quản lý tài khoản' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<MainPageUser />
+			<UpdateAccount />
 		</Fragment>
 	);
 }
 
 Page.getLayout = function (Page: ReactElement) {
-	return <BaseLayout title='Quản lý người dùng'>{Page}</BaseLayout>;
+	return <BaseLayout title='Quản lý tài khoản'>{Page}</BaseLayout>;
 };
--- src/pages/tai-khoan/index.tsx
+++ src/pages/tai-khoan/index.tsx
@@ -1,23 +1,23 @@
 import Head from 'next/head';
 import {Fragment, ReactElement} from 'react';
 import BaseLayout from '~/components/layouts/BaseLayout';
-import UpdatePageUser from '~/components/pages/nguoi-dung/UpdatePageUser';
+import MainPageAccount from '~/components/pages/tai-khoan/MainPageAccount';
 
 export default function Page() {
 	return (
 		<Fragment>
 			<Head>
-				<title>Chỉnh sửa tài khoản</title>
-				<meta name='description' content='Quản lý tài khoản' />
+				<title>Quản lý người dùng</title>
+				<meta name='description' content='Quản lý người dùng' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<UpdatePageUser />
+			<MainPageAccount />
 		</Fragment>
 	);
 }
 
 Page.getLayout = function (Page: ReactElement) {
-	return <BaseLayout title='Quản lý tài khoản'>{Page}</BaseLayout>;
+	return <BaseLayout title='Quản lý người dùng'>{Page}</BaseLayout>;
 };
