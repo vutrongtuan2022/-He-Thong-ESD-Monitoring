ffd1d36654c24b1df8f97f09db7bf9be7b7259f7
TuanVT <vutrongtuan2022@gmail.com>
2024-05-29 16:21:02 +0700

    update

--- src/pages/tai-khoan/index.tsx
+++ src/pages/tai-khoan/index.tsx
@@ -2,13 +2,14 @@ import Head from 'next/head';
 import {Fragment, ReactElement} from 'react';
 import BaseLayout from '~/components/layouts/BaseLayout';
 import MainPageAccount from '~/components/pages/tai-khoan/MainPageAccount';
+import i18n from '~/locale/i18n';
 
 export default function Page() {
 	return (
 		<Fragment>
 			<Head>
-				<title>Quản lý tài khoản</title>
-				<meta name='description' content='Quản lý tài khoản' />
+				<title>{i18n.t('Account.quanlytaikhoan')} </title>
+				<meta name='description' content={i18n.t('Account.quanlytaikhoan')} />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
@@ -19,5 +20,5 @@ export default function Page() {
 }
 
 Page.getLayout = function (Page: ReactElement) {
-	return <BaseLayout title='Quản lý tài khoản'>{Page}</BaseLayout>;
+	return <BaseLayout title={i18n.t('Account.quanlytaikhoan')}>{Page}</BaseLayout>;
 };
